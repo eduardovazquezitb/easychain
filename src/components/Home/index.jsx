cd2edc61d4d2b532ce9192ae0c056a965a6e8102
Eduardo <eduardo.vazquez.7e5@itb.cat>
2023-01-19 08:54:40 +0100

    Finished Home Page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,24 @@
+import { useState } from 'react'
+import { Frame, Title, StyledImage } from './styles'
+
 const Home = () => {
-  return <div />
+  const [hoverState, setHoverState] = useState(false)
+  return (
+    <Frame>
+      <Title
+        hover={hoverState}
+      >
+        EasyChain
+      </Title>
+      <StyledImage
+        src={process.env.PUBLIC_URL + '/EasyChainLogo.png'}
+        alt='logo'
+        onMouseEnter={() => setHoverState(true)}
+        onMouseLeave={() => setHoverState(false)}
+        hover={hoverState}
+      />
+    </Frame>
+  )
 }
 
 export default Home
