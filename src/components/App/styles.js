8f579cde118e1f93076bd3bed710e990b2e7357f
Eduardo <eduardo.vazquez.7e5@itb.cat>
2023-01-20 10:52:22 +0100

    small css fixes

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -1,17 +1,19 @@
 import styled from '@emotion/styled'
 
 export const Frame = styled.div`
-    position: absolute;
+    position: relative;
     width: 100vw;
-    height: 100vh;
+    height: fit-content;
+    min-height: 100vh;
     background-image: url('${props => props.background}'); 
+    background-repeat: repeat;
     background-size: cover;
 `
 
 export const ContentWrapper = styled.div`
     padding: 5px;
     margin: auto;
-    height: calc(100% - 3.5rem);
+    min-height: calc(100vh - 3.5rem);
     @media (min-width: ${props => props.theme.sizes.phoneSize}){
         width: calc(85% - 200px);
         padding-left: 15px;
